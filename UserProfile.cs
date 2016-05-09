6e209a27df5f441e9178357909780f9e320da4a2
Joe Coutcher <joe.coutcher@maritzcx.com>
2016-05-09 12:53:52 -0400

    Added bot_id to UserProfile

--- UserProfile.cs
+++ UserProfile.cs
@@ -8,6 +8,7 @@ namespace SlackAPI
 {
     public class UserProfile
     {
+        public string bot_id;
         public string first_name;
         public string last_name;
         public string real_name;
